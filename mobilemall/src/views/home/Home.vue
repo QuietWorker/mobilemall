416736edd36356e6b80eadb11a25558749a1277d
QuietWorker <1316461638@qq.com>
2023-04-14 15:02:25 +0800

    first commit

---
+++ mobilemall/src/views/home/Home.vue
@@ -0,0 +1,152 @@
+<template>
+  <div id="home">
+    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+    <home-swiper :banners="banners"></home-swiper>
+    <home-recommend-view :recommends="recommends"></home-recommend-view>
+    <feature-view></feature-view>
+    <tab-control class="tab-control" :titles="['最新','精选','热门']" @tabClick></tab-control>
+    <goods-list :goods="goods[currentType].list"></goods-list>
+    <ul>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+
+    </ul>
+  </div>
+</template>
+
+<script>
+import NavBar from 'components/common/navbar/NavBar.vue'
+import TabControl from 'components/content/tabControl/TabControl.vue'
+import GoodsList from 'components/content/goods/GoodsList.vue'
+
+import HomeSwiper from './childComponents/HomeSwiper.vue'
+import HomeRecommendView from './childComponents/HomeRecommendView.vue'
+import FeatureView from './childComponents/FeatureView.vue'
+
+import {getHomeMultidata,getHomeGoods} from "network/home.js"
+
+export default {
+    name:"home",
+    components:{
+      TabControl,
+      NavBar,
+      GoodsList,
+      HomeSwiper,
+      HomeRecommendView,
+      FeatureView
+    },
+    data(){
+      return{
+        banners:[],
+        recommends:[],
+        goods:{
+          'pop':{page:0,list:[]},
+          'new':{page:0,list:[]},
+          'sell':{page:0,list:[]}
+        },
+        currentType:'pop'
+      }
+    },
+    created(){
+        // 1.请求多个数据
+        this.getHomeMultidata(),
+        // 2.请求商品数据
+        this.getHomeGoods('pop')
+        this.getHomeGoods('new')
+        this.getHomeGoods('sell')
+    },
+    methods:{
+      tabClick(index){
+        switch(index){
+          case 0:
+            this.currentType='pop'
+            break
+          case 1:
+            this.currentType='new'
+            break
+          case 2:
+            this.currentType='sell'
+            break
+            //case有穿透
+        }
+      },
+      getHomeMultidata(){
+        getHomeMultidata().then(res=>{
+        // this.result=res
+        this.banners=res.data.banner.list
+        this.recommends=res.data.recommend.list
+      })
+      },
+      getHomeGoods(type){
+        const page=this.goods[type].page+1
+        getHomeGoods(type,page).then(res=>{
+          this.goods[type].list.push(...res.data.list)
+          this.goods[type].page+=1
+      })
+      }
+    }
+}
+
+</script>
+
+<style>
+  #home{
+    padding-top: 44px;
+
+  }
+  .home-nav{
+    background-color: var(--color-tint);
+    color: #fff;
+    position: fixed;
+    left: 0;
+    right: 0;
+    top:0;
+    z-index: 9;
+  }
+  .tab-control{
+    position: sticky;
+    top: 44px;
+    background-color: #fff;
+    z-index: 9;
+  }
+</style>
